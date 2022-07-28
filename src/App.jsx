4f27741e5f3106a2c6be0261c3b42f4e9d699570
Rodriman28 <35462876+Rodriman28@users.noreply.github.com>
2022-07-28 19:22:57 -0300

    Little changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import EditarCliente from "./pages/EditarCliente";
 import VerCliente from "./pages/VerCliente";
 
 function App() {
+  console.log(import.meta.env.VITE_API_URL);
+
   return (
     <BrowserRouter>
       <Routes>
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -7,7 +7,7 @@ const Inicio = () => {
   useEffect(() => {
     const obtenerClientesAPI = async () => {
       try {
-        const url = "http://localhost:4000/clientes";
+        const url = import.meta.env.VITE_API_URL;
         const respuesta = await fetch(url);
         const resultado = await respuesta.json();
 
