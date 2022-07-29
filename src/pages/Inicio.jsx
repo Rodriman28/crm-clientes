1478e1d85b2dbc4f30d88df9a7309fd1f1407af0
Rodriman28 <35462876+Rodriman28@users.noreply.github.com>
2022-07-29 10:25:33 -0300

    Env variables

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -23,7 +23,7 @@ const Inicio = () => {
     const confirmar = confirm("Deseas eliminar este cliente?");
     if (confirmar) {
       try {
-        const url = `http://localhost:4000/clientes/${id}`;
+        const url = `${import.meta.env.VITE_API_URL}/${id}`;
         const respuesta = await fetch(url, {
           method: "Delete",
         });
