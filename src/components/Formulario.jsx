8731f03cd4d151b0097e215f30c5e5833c3ab41b
Rodriman28 <35462876+Rodriman28@users.noreply.github.com>
2022-07-29 11:10:21 -0300

    Path correction

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -49,7 +49,7 @@ const Formulario = ({ cliente, cargando }) => {
       }
       await respuesta.json();
 
-      navigate("/clientes");
+      navigate("/");
     } catch (error) {
       console.error(error);
     }
